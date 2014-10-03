aed9429bc66da70d5333eb384a4c9559b9c64f49
Venkata Chaitanya Goli <vgoli@mediamath.com>
2014-10-03 11:57:24 -0500

    Update back-todo.js
    The new .js file is added after taking in the suggestion from the code review from the team.

--- back-todo/back-todo.js
+++ back-todo/back-todo.js
@@ -1,6 +1,3 @@
-var itemChecked = false;
-Backbone.View.prototype.event_aggregator = _.extend({}, Backbone.Events);
-
 var Todoitem = Backbone.Model.extend({
 	defaults: {
 		checked: false,
@@ -8,82 +5,28 @@ var Todoitem = Backbone.Model.extend({
 	}
 });
 
-var todoitem = new Todoitem();
-
 var Items = Backbone.Collection.extend({
 	model: Todoitem,
 });
 
-var items = new Items();
-
-var ItemList = Backbone.View.extend({
-
-	el:'.mainContainer',
+var InputView = Backbone.View.extend({
+	el:'.inputContainer',
 
 	initialize: function(){
-		this.event_aggregator.bind("toggleState", this.toggleState);
-	},
-
-	render: function(){
-		var todoName = $('#todoItem').val();
-		var template = _.template($('#todo-list-template').html(),{items: items.models});
-		$('#itemContainer').html(template);
-		this.toggleState();
-		footer.render();
+		items = new Items();
+		router = new Router();
+		itemView = new ItemView();
+		footer = new FooterTodo();
 	},
 
 	events:{
-
-		'click mm-checkbox' : 'checkedItems',
-		'click mm-icon' : 'deleteTodo',
 		'keypress mm-input': 'pressEnter',
 		'click mm-button':'addTodoItem',
-		'click #toggleCheck': 'toggleAll',
-		'dblclick #itemInfo': 'enableEdit',
-		'keypress span': 'endEdit',
-		'blur span':'saveEdit'
+		'click #toggleCheck': 'toggleAll'	
 	},
-
-	activeTodoItems: function(){
-		var activeTodo = _.filter(items.models,function(model){return !model.attributes.checked});
-		var template = _.template($('#todo-list-template').html(),{items: activeTodo});
-		$('#itemContainer').html(template);	
-	},
-
-	completedTodoItems: function(){
-		var completeTodo = _.filter(items.models,function(model){return model.attributes.checked});
-		var template = _.template($('#todo-list-template').html(),{items: completeTodo});
-		$('#itemContainer').html(template);
-	},
-
-	checkedItems: function(e){
-		var chkid = e.target.dataset.id;
-		itemChecked = e.target.checked;
-
-		if(itemChecked){
-			var i = items.length;
-			while(i--){
-					if(items.models[i].cid == chkid){
-						items.models[i].set('checked' , itemChecked);
-					}
-			}
-		}
-		else{
-			var i = items.length;
-			while(i--){
-					if(items.models[i].cid == chkid){
-						items.models[i].set('checked' ,itemChecked);
-					}
-			}
-		}
-
-		footer.render();
-		itemList.render();
-		router.navigate('#/all' , {trigger: true});	
-	},
-
+	
 	toggleState: function(){
-		var completeTodo = _.filter(items.models,function(model){return model.attributes.checked}).length;
+		var completeTodo = items.where({checked: true}).length;
 		var listLength = items.models.length;
 
 		if(completeTodo === listLength){
@@ -92,30 +35,18 @@ var ItemList = Backbone.View.extend({
 		else if(completeTodo < listLength && completeTodo != 0){
 			$('#toggleCheck').attr('state','partial');
 		}
-		else if(completeTodo == 0){
+		else if(completeTodo === 0){
 			$('#toggleCheck').attr('state','unchecked');
 		}
 	},
 
 	toggleAll: function(e){
-		var completeTodo = _.filter(items.models,function(model){return model.attributes.checked});
-		var i = items.models.length;
 		var toggleBox = e.target.checked;
-
-		if(toggleBox){
-			while(i--){
-				items.models[i].set('checked', true);
-			}	
-		}
-		else{
-			var i = items.models.length;
-			while(i--){
-				items.models[i].set('checked', false);
-			}
-		}
-
-		itemList.render();
-		footer.render();
+		
+		items.each( function(model){
+			model.set("checked", toggleBox);
+		});
+		router.navigate('#/all' , {trigger: true});
 	},
 
 	pressEnter: function(e){
@@ -126,15 +57,65 @@ var ItemList = Backbone.View.extend({
 
 	addTodoItem: function(){
 		var todoName = $('#todoItem');
-		if (todoName.val() == "" || todoName.val() == " ") {
-			return false; 
+	
+		if( $.trim(todoName.val()).length > 0 ){
+			items.add({name:todoName.val()});
+			todoName.val("");
+			router.navigate('#/all' , {trigger: true});
 		}
+		else{
+			todoName.val("");
+		}		
+	}	
+});
 
-		items.add({name:todoName.val()});
-		itemList.render();
-		footer.render();
-		this.toggleState();
-		todoName.val("");
+var ItemView = Backbone.View.extend({
+	el:'.itemContainer',
+
+	events: {
+		'click mm-checkbox' : 'checkedItems',
+		'click mm-icon' : 'deleteTodo',
+		'dblclick #itemInfo': 'enableEdit',
+		'keypress span': 'endEdit',
+		'blur span':'saveEdit'
+	},
+	
+	render: function(){
+		var todos = new Items();
+		var template = _.template($('#todo-list-template').html(),{items: items.models});
+		this.$el.html(template);
+		this.listenTo(todos, 'change', footer.render());
+		this.listenTo(todos, 'change' , inputView.toggleState());
+	},
+
+	checkedItems: function(e){
+		var chkid = e.target.dataset.id;
+		var itemChecked = e.target.checked;
+
+		if(itemChecked){
+			var i = items.length;
+			while(i--){
+				if(items.models[i].cid == chkid){
+					items.models[i].set('checked' , itemChecked);
+				}
+			}
+		}
+		else{
+			var i = items.length;
+			while(i--){
+				if(items.models[i].cid == chkid){
+					items.models[i].set('checked' ,itemChecked);
+				}
+			}
+		}
+		router.navigate('#/all' , {trigger: true});
+
+	},
+
+	deleteTodo: function(e){
+		var delId = e.target.dataset.id;
+		delItem = items.get(delId);
+		items.remove(delItem);
 		router.navigate('#/all' , {trigger: true});
 	},
 
@@ -147,38 +128,17 @@ var ItemList = Backbone.View.extend({
 			this.saveEdit(e);
 		}	
 	},
+
 	saveEdit: function(e){
 		var editId = e.target.dataset.id;
-		var i = items.length;
-
-			while(i--){
-				if(items.models[i].cid == editId){
-				items.models[i].attributes.name = e.target.innerHTML;
-				}
-			}
-
+		var editItem = items.get(editId);
+		editItem.set("name", e.target.innerHTML);
 		e.target.contentEditable="false";
 		console.log('saved');	
-	},
-
-	deleteTodo: function(e){
-		var delId = e.target.dataset.id;
-		var i = items.length;
-
-		while(i--){
-			if(items.models[i].cid == delId){
-				items.remove(items.at(i));	
-			}
-		}
-		
-		footer.render();
-		this.toggleState();
-		router.navigate('#/all' , {trigger: true});	
 	}
 });
 
 var FooterTodo = Backbone.View.extend({
-
 	el:'.todoFooter',
 
 	events: {
@@ -186,20 +146,30 @@ var FooterTodo = Backbone.View.extend({
 	},
 	
 	render: function() {
-		var completeTodo = _.filter(items.models,function(model){return model.attributes.checked});
-		var activeTodo = _.filter(items.models,function(model){return !model.attributes.checked});
+		var completeTodo = items.where({checked: true});
+		var activeTodo = items.where({checked: false});
 		compItems = completeTodo.length;
 		actItems = activeTodo.length;
 		var template = _.template($('#todo-footer-template').html(),{checkedItems: compItems,remainingItems: actItems});
 		this.$el.html(template);	
 	},
 
+	activeTodoItems: function(){
+		var activeTodo = items.where({checked: false});
+		var template = _.template($('#todo-list-template').html(),{items: activeTodo});
+		$('.itemContainer').html(template);	
+	},
+
+	completedTodoItems: function(){
+		var completeTodo = items.where({checked: true});
+		var template = _.template($('#todo-list-template').html(),{items: completeTodo});
+		$('.itemContainer').html(template);
+	},
+
 	clearItems: function(){
 		var completeTodo = _.filter(items.models,function(model){return model.attributes.checked});
 		items.remove(completeTodo);
-		footer.render();
 		router.navigate('#/all' , {trigger: true});
-		this.event_aggregator.trigger("toggleState");
 	}
 });
 
@@ -212,23 +182,22 @@ var Router = Backbone.Router.extend({
 	}
 });
 
-var itemList = new ItemList();
-var footer = new FooterTodo();
-var router = new Router();
+inputView = new InputView();
 
 router.on('route:home',function(){
-	itemList.render();
-	footer.render();
+	itemView.render();
 });
 
 router.on('route:activeItems',function(){
-	itemList.activeTodoItems();
+	footer.activeTodoItems();
 });
 
 router.on('route:completedItems',function(){
-	itemList.completedTodoItems();
+	footer.completedTodoItems();
 });
 
 Backbone.history.start();
+
+
 
 
