a3b072a27ace64283a702e0320132def110d8182
chaitugoli <vgoli@MM-MAC-0718.local>
2014-10-22 15:47:15 -0500

    updates

--- Api Assignment/api.js
+++ Api Assignment/api.js
@@ -1,8 +1,4 @@
  Polymer("ajax-test", {
-	url:'http://challenge.mediamath.com/api/agencies'+'?api_token=dd88982667b3ec6896d6a3a50d80efa3faea6923',
-	urladv:'http://challenge.mediamath.com/api/advertisers'+'?api_token=dd88982667b3ec6896d6a3a50d80efa3faea6923',
-	urlcam:'http://challenge.mediamath.com/api/campaigns'+'?api_token=dd88982667b3ec6896d6a3a50d80efa3faea6923',
-	text1:'',
 	urlObject:[],
 	advObject:[],
 	camObject:[],
@@ -15,174 +11,147 @@
 	agencySelected:'',
 	agencyID:'',
 	advId:'',
-	campStatus:'',
 
-	
 	agenId: function(){
 		var i = this.agencies.length;
 		while(i--){
 			if(this.agencies[i].name == this.agencySelected){
 				this.agencyID = this.agencies[i]._id;
 			}
-		}  
-		
+		} 
 	},
 
 	changeStatus: function(e){
-			console.log(e);
-				var i = this.campNames.length;
-				while(i--){
-					if(this.campNames[i]._id == e.target.dataset.id){
-						if(e.target.value == "active"){
-							this.campNames[i].status = true;  
-						}  
-						else if(e.target.value == "inactive"){
-						this.campNames[i].status= false;
-					} 
-					}
-					
-					
-				}
-	},
-	
-	getValue: function(){
-		return "active";
-		console.log("active");
+		var i = this.campNames.length;
+		while(i--){
+			if(this.campNames[i]._id == e.target.dataset.id){
+				if(e.target.value === "active"){
+					this.campNames[i].status = true;  
+				}  
+				else if(e.target.value === "inactive"){
+					this.campNames[i].status = false;
+				} 
+			}	
+		}	
 	},
 
  	getAdv: function(){
-		this.agenId();
 		this.$.a2.exec();
-		this.$.advList.placeholder="advertisers Names";
+		this.agenId();
+		this.$.advList.placeholder = "advertisers Names";
 		this.advNames = [];
 		var i = this.advertisers.length;
 		while(i--){
-			if(this.advertisers[i].agency_id == this.agencyID){
-				this.advNames.push({name:this.advertisers[i].name ,id:this.advertisers[i]._id});
-				console.log(this.advertisers[i].name);
-				
+			if(this.advertisers[i].agency_id === this.agencyID){
+				this.advNames.push({name:this.advertisers[i].name ,id:this.advertisers[i]._id});	
 			}
 		}
-
-		 
 	},
 	
-	getAdvId: function() {
-		 
+	getAdvId: function(){ 
 		var i = this.advNames.length
 		while(i--){
-			if(this.advNames[i].name == this.$.advList.value){
+			if(this.advNames[i].name === this.$.advList.value){
 				 this.advId = this.advNames[i].id;
 			}
-		}
-		
+		}	
 	},
 
-	
-	
-	getCamp:function(){
+	getCamp: function(){
 		this.getAdvId();
 		this.campNames = [];
 
-		if(this.$.advList.value==""){
+		if(this.$.advList.value === ""){
 				return false
 		}	 
-		var i= this.campaigns.length;
+		var i = this.campaigns.length;
 		while(i-- ){
-			if(this.advId == this.campaigns[i].advertiser_id){
+			if(this.advId === this.campaigns[i].advertiser_id){
 				this.changeDate(i);
-			 this.campNames.push(this.campaigns[i]); 
-						 
-						 
-			}
-						 						 
-				}
+			 	this.campNames.push(this.campaigns[i]);			 
+			}		 						 
+		}
 
-		console.log(this.campNames);
 		this.toggleCheck();
-		
 		this.$.gridTable.hidden = false;
+		this.$.footerInfo.hidden = false;
 		this.getStatus();
 		this.async(function(){
 				$('#startDatePicker::shadow #input').attr("type","date");
 				$('#endDatePicker::shadow #input').attr("type","date");
-		});
-		
+		});	
 	},
-	getStatus: function(e) {
 
+	getStatus: function(e) {
 		var i = this.campNames.length;
 		while(i--){
-		if(this.campNames[i].status){
-			this.campNames[i].campStatus ="active";
+			if(this.campNames[i].status){
+				this.campNames[i].campStatus = "active";
+			}
+			else if(!this.campNames[i].status){
+				this.campNames[i].campStatus = "inactive";
+			}
 		}
-		else if(!this.campNames[i].status){
-			this.campNames[i].campStatus ="inactive";
-		}
-	}
-		},
-
+	},
 
 	changeDate: function(i){
-			var sDate = new Date(this.campaigns[i].start_date);
-			var sMonth = (0+(sDate.getMonth()+1).toString()).substr(-2) ;
-			var sDay = (0+sDate.getDate().toString()).substr(-2);
-			var sD = sDate.getFullYear()+"-"+sMonth+"-"+sDay; 
-			// this.campNames.push({startDate: sD});
-
-		 	this.campaigns[i].startDate = sD;
-			var eDate = new Date(this.campaigns[i].end_date);
-			var eMonth = (0+(eDate.getMonth()+1).toString()).substr(-2);
-			var eDay = (0+eDate.getDate().toString()).substr(-2);
-			var eD = eDate.getFullYear()+"-"+eMonth+"-"+eDay
-			this.campaigns[i].endDate = eD;
-			 //this.campNames.push({endDate: eD});
+		var sDate = new Date(this.campaigns[i].start_date);
+		var sMonth = (0+(sDate.getMonth()+1).toString()).substr(-2) ;
+		var sDay = (0+sDate.getDate().toString()).substr(-2);
+		var sD = sDate.getFullYear()+"-"+sMonth+"-"+sDay; 
+	 	this.campaigns[i].startDate = sD;
+		var eDate = new Date(this.campaigns[i].end_date);
+		var eMonth = (0+(eDate.getMonth()+1).toString()).substr(-2);
+		var eDay = (0+eDate.getDate().toString()).substr(-2);
+		var eD = eDate.getFullYear()+"-"+eMonth+"-"+eDay
+		this.campaigns[i].endDate = eD;
 	},
 
 	toggleCheck: function(){
-			var campLength = this.campNames.length;
-			var campChecked = this.campNames.filter(function(cam){return cam.checked}).length;
+		var campLength = this.campNames.length;
+		var campChecked = this.campNames.filter(function(cam){return cam.checked}).length;
 
-	if (campChecked === campLength) {
-			 this.$.toggleAllCheck.checked = true;
-	}
-	else if (campChecked < campLength && campChecked >0 ) {
-			 this.$.toggleAllCheck.state="partial";
-	}
-	else if (campChecked === 0 ) {
-			this.$.toggleAllCheck.checked= false;
-	}
-	else if ( campLength === 0 ) {
+		if (campChecked === campLength) {
 			this.$.toggleAllCheck.checked = true;
-	}
+			this.$.campSave.disabled = false;
+		}
+		else if (campChecked < campLength && campChecked >0 ) {
+			this.$.toggleAllCheck.state="partial";
+			this.$.campSave.disabled = false;
+		}
+		else if (campChecked === 0 ) {
+			this.$.toggleAllCheck.checked= false;
+			this.$.campSave.disabled = true;
+		}
+		else if ( campLength === 0 ) {
+			this.$.toggleAllCheck.checked = true;
+			this.$.campSave.disabled = true;
+		}
+				
 	},
 
 	checkAll: function() {
-			var i = this.campNames.length;
+		var i = this.campNames.length;
 
-	if(this.$.toggleAllCheck.checked === true) {
+		if(this.$.toggleAllCheck.checked === true) {
 			while (i--) {
-					this.campNames[i].checked = true;
+				this.campNames[i].checked = true;
 			}
-	}
+		}
 
-	if(this.$.toggleAllCheck.checked === false) {
+		if(this.$.toggleAllCheck.checked === false) {
 			while (i--) {
-					this.campNames[i].checked = false;
+				this.campNames[i].checked = false;
 			}
-	}
-
+		}
 	},
 
 	responseChanged: function(_old, _new) {
-
 		this.urlObject = this.response;
-		this.agencies = this.urlObject.agencies;
-			
+		this.agencies = this.urlObject.agencies;		
 	},
 	
 	responseAdvChanged: function(_old, _new){
-
 		this.advObject = this.responseAdv;
 		this.advertisers = this.advObject.advertisers;
 	},
@@ -194,50 +163,30 @@
 
 	saveCampaign: function() {
 
-				this.changedCampNames =[];
-		
-			var i = this.campNames.length;
-			if(this.$.toggleAllCheck.state==="unchecked"){
-				alert("please select the Campaigns you want to save!! ");
-			}
-			while(i--){
-				if(this.campNames[i].checked){
-					this.campID=this.campNames[i]._id;
-					this.updateCamp(i); 
-					console.log(this.campNames);
-					a = document.createElement("mm-ajax");
-					a.setAttribute("url","http://challenge.mediamath.com/api/campaigns/"+this.campID+"?api_token=dd88982667b3ec6896d6a3a50d80efa3faea6923");
-					a.setAttribute("method","POST");
-					a.body = this.campNames[i];
-					a.exec();  
-					alert(this.campNames[i].name+"  is Saved!!!");
+		this.changedCampNames = [];
 	
-				}
-
+		var i = this.campNames.length;
+	
+		while(i--){
+			if(this.campNames[i].checked){
+				this.campID = this.campNames[i]._id;
+				this.updateCamp(i); 
+				var a = document.createElement("mm-ajax");
+				a.setAttribute("url","http://challenge.mediamath.com/api/campaigns/"+this.campID+"?api_token=dd88982667b3ec6896d6a3a50d80efa3faea6923");
+				a.setAttribute("method","POST");
+				a.body = this.campNames[i];
+				a.exec();  	
 			}
-			
-
-
-	},
-
-	checkChange: function(masterCamp) {
-		var masterCamp = this.camObject.campaigns;
-		var i = this.changedCampNames.length;
-			while(i--){
-				 if(masterCamp[i].budget==this.changedCampNames[i].budget && masterCamp[i].name==this.changedCampNames[i].name && masterCamp[i].end_date==this.changedCampNames[i].end_date && masterCamp[i].start_date==this.changedCampNames[i].start_date){
-							console.log("No change");
-				 }
-				 else {
-					 console.log(this.campNames[i] , "Changed Campaign");
-				 }
-			}
+		}
+		alert("The Selected Campaigns Are Saved!!!");
 	},
 
 	updateCamp: function(i){
-			var newSdate = new Date(this.campNames[i].startDate).toISOString();
-			var newEdate = new Date(this.campNames[i].endDate).toISOString();
-			this.campNames[i].start_date = newSdate;
-			this.campNames[i].end_date = newEdate;
+		var newSdate = new Date(this.campNames[i].startDate).toISOString();
+		var newEdate = new Date(this.campNames[i].endDate).toISOString();
+		this.campNames[i].start_date = newSdate;
+		this.campNames[i].end_date = newEdate;
+		this.changeDate(i);
 	}
 
 });
--- back-todo/back-todo.js
+++ back-todo/back-todo.js
@@ -1,198 +1,206 @@
-var Todoitem = Backbone.Model.extend({
-	defaults: {
-		checked: false,
-		name: ''
-	}
-});
-
-var Items = Backbone.Collection.extend({
-	model: Todoitem,
-});
-
-var InputView = Backbone.View.extend({
-	el:'.inputContainer',
-
-	initialize: function(){
-		items = new Items();
-		router = new Router();
-		itemView = new ItemView();
-		footer = new FooterTodo();
-	},
-
-	events:{
-		'keypress mm-input': 'pressEnter',
-		'click mm-button':'addTodoItem',
-		'click #toggleCheck': 'toggleAll'	
-	},
+$(function() {
+	var items;
+	var router;
+	var itemView;
+	var footer; 
+	var inputView;
 	
-	toggleState: function(){
-		var completeTodo = items.where({checked: true}).length;
-		var listLength = items.models.length;
+	var Todoitem = Backbone.Model.extend({
+		defaults: {
+			checked: false,
+			name: ''
+		}
+	});
 
-		if(completeTodo === listLength){
-			$('#toggleCheck').attr('state','checked');
-		}
-		else if(completeTodo < listLength && completeTodo != 0){
-			$('#toggleCheck').attr('state','partial');
-		}
-		else if(completeTodo === 0){
-			$('#toggleCheck').attr('state','unchecked');
-		}
-	},
+	var Items = Backbone.Collection.extend({
+		model: Todoitem,
+	});
 
-	toggleAll: function(e){
-		var toggleBox = e.target.checked;
+	var InputView = Backbone.View.extend({
+		el:'.inputContainer',
+
+		initialize: function(){
+			items = new Items();
+			router = new Router();
+			itemView = new ItemView();
+			footer = new FooterTodo();
+		},
+
+		events:{
+			'keypress mm-input': 'pressEnter',
+			'click mm-button':'addTodoItem',
+			'click #toggleCheck': 'toggleAll'	
+		},
 		
-		items.each( function(model){
-			model.set("checked", toggleBox);
-		});
-		router.navigate('#/all' , {trigger: true});
-	},
+		toggleState: function(){
+			var completeTodo = items.where({checked: true}).length;
+			var listLength = items.models.length;
 
-	pressEnter: function(e){
-		if(e.which==13){
-			this.addTodoItem();
+			if(completeTodo === listLength){
+				$('#toggleCheck').attr('state','checked');
+			}
+			else if(completeTodo < listLength && completeTodo != 0){
+				$('#toggleCheck').attr('state','partial');
+			}
+			else if(completeTodo === 0){
+				$('#toggleCheck').attr('state','unchecked');
+			}
+		},
+
+		toggleAll: function(e){
+			var toggleBox = e.target.checked;
+			
+			items.each( function(model){
+				model.set("checked", toggleBox);
+			});
+			router.navigate('#/all' , {trigger: true});
+		},
+
+		pressEnter: function(e){
+			if(e.which==13){
+				this.addTodoItem();
+			}
+		},
+
+		addTodoItem: function(){
+			var todoName = $('#todoItem');
+		
+			if( $.trim(todoName.val()).length > 0 ){
+				items.add({name:todoName.val()});
+				todoName.val("");
+				router.navigate('#/all' , {trigger: true});
+			}
+			else{
+				todoName.val("");
+			}		
+		}	
+	});
+
+	var ItemView = Backbone.View.extend({
+		el:'.itemContainer',
+
+		events: {
+			'click mm-checkbox' : 'checkedItems',
+			'click mm-icon' : 'deleteTodo',
+			'dblclick #itemInfo': 'enableEdit',
+			'keypress span': 'endEdit',
+			'blur span':'saveEdit'
+		},
+		
+		render: function(){
+			var todos = new Items();
+			var template = _.template($('#todo-list-template').html(),{items: items.models});
+			this.$el.html(template);
+			this.listenTo(todos, 'change', footer.render());
+			this.listenTo(todos, 'change' , inputView.toggleState());
+		},
+
+		checkedItems: function(e){
+			var chkid = e.target.dataset.id;
+			var itemChecked = e.target.checked;
+
+			items.each( function(model){
+				if(model.cid == chkid){
+					model.set("checked", itemChecked);
+				}
+			});
+
+			router.navigate('#/all' , {trigger: true});
+
+		},
+
+		deleteTodo: function(e){
+			var delId = e.target.dataset.id;
+			delItem = items.get(delId);
+			items.remove(delItem);
+			router.navigate('#/all' , {trigger: true});
+		},
+
+		enableEdit: function(e){
+			e.target.contentEditable="true";
+		},
+
+		endEdit: function(e){
+			if(e.which === 13){
+				this.saveEdit(e);
+			}
+		},
+
+		saveEdit: function(e){
+			var editId = e.target.dataset.id;
+			var editItem = items.get(editId);
+			var changedItem = e.target.innerText;
+			editItem.set("name", changedItem);
+			e.target.contentEditable="false";
+			console.log('saved');	
+			
+			if($.trim(changedItem).length == 0 ){
+				this.deleteTodo(e);
+			}
+
 		}
-	},
+		
+	});
 
-	addTodoItem: function(){
-		var todoName = $('#todoItem');
-	
-		if( $.trim(todoName.val()).length > 0 ){
-			items.add({name:todoName.val()});
-			todoName.val("");
+	var FooterTodo = Backbone.View.extend({
+		el:'.todoFooter',
+
+		events: {
+			'click mm-button' : 'clearItems'
+		},
+		
+		render: function() {
+			var completeTodo = items.where({checked: true});
+			var activeTodo = items.where({checked: false});
+			compItems = completeTodo.length;
+			actItems = activeTodo.length;
+			var template = _.template($('#todo-footer-template').html(),{checkedItems: compItems,remainingItems: actItems});
+			this.$el.html(template);	
+		},
+
+		activeTodoItems: function(){
+			var activeTodo = items.where({checked: false});
+			var template = _.template($('#todo-list-template').html(),{items: activeTodo});
+			$('.itemContainer').html(template);	
+		},
+
+		completedTodoItems: function(){
+			var completeTodo = items.where({checked: true});
+			var template = _.template($('#todo-list-template').html(),{items: completeTodo});
+			$('.itemContainer').html(template);
+		},
+
+		clearItems: function(){
+			var completeTodo = items.where({checked: true});
+			items.remove(completeTodo);
 			router.navigate('#/all' , {trigger: true});
 		}
-		else{
-			todoName.val("");
-		}		
-	}	
-});
+	});
 
-var ItemView = Backbone.View.extend({
-	el:'.itemContainer',
-
-	events: {
-		'click mm-checkbox' : 'checkedItems',
-		'click mm-icon' : 'deleteTodo',
-		'dblclick #itemInfo': 'enableEdit',
-		'keypress span': 'endEdit',
-		'blur span':'saveEdit'
-	},
-	
-	render: function(){
-		var todos = new Items();
-		var template = _.template($('#todo-list-template').html(),{items: items.models});
-		this.$el.html(template);
-		this.listenTo(todos, 'change', footer.render());
-		this.listenTo(todos, 'change' , inputView.toggleState());
-	},
-
-	checkedItems: function(e){
-		var chkid = e.target.dataset.id;
-		var itemChecked = e.target.checked;
-
-		items.each( function(model){
-			if(model.cid == chkid){
-				model.set("checked", itemChecked);
-			}
-		});
-
-		router.navigate('#/all' , {trigger: true});
-
-	},
-
-	deleteTodo: function(e){
-		var delId = e.target.dataset.id;
-		delItem = items.get(delId);
-		items.remove(delItem);
-		router.navigate('#/all' , {trigger: true});
-	},
-
-	enableEdit: function(e){
-		e.target.contentEditable="true";
-	},
-
-	endEdit: function(e){
-		if(e.which === 13){
-			this.saveEdit(e);
-		}	
-	},
-
-	saveEdit: function(e){
-		var editId = e.target.dataset.id;
-		var editItem = items.get(editId);
-		var changedItem = e.target.innerHTML;
-		editItem.set("name", changedItem);
-		e.target.contentEditable="false";
-		console.log('saved');	
-		
-		if(changedItem.length== 0 ){
-			this.deleteTodo(e);
+	var Router = Backbone.Router.extend({
+		routes: {
+			'' : 'home',
+			'active': 'activeItems',
+			'completed': 'completedItems',
+			'all': 'home'
 		}
-	}
+	});
+
+	inputView = new InputView();
+
+	router.on('route:home',function(){
+		itemView.render();
+	});
+
+	router.on('route:activeItems',function(){
+		footer.activeTodoItems();
+	});
+
+	router.on('route:completedItems',function(){
+		footer.completedTodoItems();
+	});
+
+	Backbone.history.start();
 });
-
-var FooterTodo = Backbone.View.extend({
-	el:'.todoFooter',
-
-	events: {
-		'click mm-button' : 'clearItems'
-	},
-	
-	render: function() {
-		var completeTodo = items.where({checked: true});
-		var activeTodo = items.where({checked: false});
-		compItems = completeTodo.length;
-		actItems = activeTodo.length;
-		var template = _.template($('#todo-footer-template').html(),{checkedItems: compItems,remainingItems: actItems});
-		this.$el.html(template);	
-	},
-
-	activeTodoItems: function(){
-		var activeTodo = items.where({checked: false});
-		var template = _.template($('#todo-list-template').html(),{items: activeTodo});
-		$('.itemContainer').html(template);	
-	},
-
-	completedTodoItems: function(){
-		var completeTodo = items.where({checked: true});
-		var template = _.template($('#todo-list-template').html(),{items: completeTodo});
-		$('.itemContainer').html(template);
-	},
-
-	clearItems: function(){
-		var completeTodo = _.filter(items.models,function(model){return model.attributes.checked});
-		items.remove(completeTodo);
-		router.navigate('#/all' , {trigger: true});
-	}
-});
-
-var Router = Backbone.Router.extend({
-	routes: {
-		'' : 'home',
-		'active': 'activeItems',
-		'completed': 'completedItems',
-		'all': 'home'
-	}
-});
-
-inputView = new InputView();
-
-router.on('route:home',function(){
-	itemView.render();
-});
-
-router.on('route:activeItems',function(){
-	footer.activeTodoItems();
-});
-
-router.on('route:completedItems',function(){
-	footer.completedTodoItems();
-});
-
-Backbone.history.start();
-
-
 
 
